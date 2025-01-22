a8ab4f5961e6b1ab7add2eee649fa3a6d8c30459
humzah286 <humzah286@gmail.com>
2025-01-23 01:26:30 +0500

    Connecting Redis to backend

--- backend/main.py
+++ backend/main.py
@@ -7,6 +7,24 @@ from contextlib import asynccontextmanager
 import hashlib
 import time
 import os
+import redis.asyncio as redis
+
+
+DATABASE_URL = os.getenv("MONGO_URI")
+REDIS_HOST = os.getenv("REDIS_HOST")
+REDIS_PORT = os.getenv("REDIS_PORT")
+
+
+async def startup_redis():
+    global redis_client
+    redis_host = REDIS_HOST  # Use the service name defined in docker-compose
+    redis_port = REDIS_PORT
+    redis_client = redis.Redis(host=redis_host, port=redis_port, decode_responses=True)
+    try:
+        await redis_client.ping()  # Test connection
+        print("Connected to Redis!")
+    except redis.exceptions.ConnectionError:
+        print("Could not connect to Redis!")
 
 
 class Item(BaseModel):
@@ -23,12 +41,10 @@ class ItemUpdate(BaseModel):
     description: Optional[str]
 
 
-DATABASE_URL = os.getenv("MONGO_URI")
-
-
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # Startup:
+    startup_redis()
     app.mongodb_client = MongoClient(DATABASE_URL)
     app.mongodb = app.mongodb_client["mydatabase"]
     yield
@@ -62,5 +78,18 @@ def list_items():
         item["id"] = str(item.pop("_id"))  # Rename _id to id and convert to string
         items.append(item)
     return {"items": items}
+
+
+@app.post("/cache/")
+async def set_cache(key: str, value: str):
+    await redis_client.set(key, value, ex=3600)  # Store value with a 1-hour expiration
+    return {"message": f"Key '{key}' set with value '{value}'"}
+
+@app.get("/cache/{key}")
+async def get_cache(key: str):
+    value = await redis_client.get(key)
+    if value is None:
+        return {"message": "Key not found"}
+    return {"key": key, "value": value}
 
 
