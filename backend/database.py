79c5ccb35e41e2e7f979b9f761a2727fd4058a53
humzah286 <humzah286@gmail.com>
2025-01-28 01:41:51 +0500

    Added unique index on email

--- backend/database.py
+++ backend/database.py
@@ -4,6 +4,7 @@ import os
 DATABASE_URL = os.getenv("MONGO_URI")
 
 mongoClient = MongoClient(DATABASE_URL)
+mongoClient["mydatabase"]["users"].create_index("email", unique=True)
 
 def get_db():
     return mongoClient
